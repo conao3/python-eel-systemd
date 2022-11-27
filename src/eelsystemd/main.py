97dd7654ac50f93dbf57d3c0bb42f2d4c31d3ac9
Naoya Yamashita <conao3@gmail.com>
2022-11-28 01:13:30 +0900

    depends on jc

--- src/eelsystemd/main.py
+++ src/eelsystemd/main.py
@@ -1,10 +1,19 @@
+import subprocess
+from typing import Any
 import eel
+import jc
 
 
 @eel.expose
 def hello(msg: str):
     print(f"Hello from Python.  {msg=}")
 
+
+@eel.expose
+def py_systemctl() -> list[dict[str, Any]]:
+    print('py_systemctl')
+    proc = subprocess.run(['systemctl'], check=True, capture_output=True, text=True)
+    return jc.parse('systemctl', proc.stdout)
 
 def run():
     print('hello')
